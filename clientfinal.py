36787fc83517ce7dc8dc233327ffc60ceddd55f1
mynameisashllee <akshara.mantha07@rafflesgirlssch.edu.sg>
2024-06-04 13:55:06 +0800

    it works but the server doesn't receive encrypted image (sick and twisted)

--- clientfinal.py
+++ clientfinal.py
@@ -1,44 +1,72 @@
-#!/usr/bin/python3
-# TCP client example
 import socket
 import os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+from PIL import Image
+import imagehash
+
+# Encryption key (must be 16, 24, or 32 bytes long)
+key = b'Sixteen byte key'
+iv = get_random_bytes(16)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("localhost", 5005))
-k = ' '
-size = 1024
+
+def encrypt_image(fname):
+    with open(fname, 'rb') as fp:
+        original_data = fp.read()
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        encrypted_data = cipher.encrypt(pad(original_data, AES.block_size))
+    return encrypted_data
+
+def hash_image(fname, hashfunc=imagehash.phash):
+    img = Image.open(fname)
+    return hashfunc(img)
+
+def decrypt_image(encrypted_data):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
+    return decrypted_data
 
 while True:
-    print("Do you want to transfer a \n1.Text File\n2.Image\n3.Video\n")
-    k = input()
-    client_socket.send(k.encode())
-    k = int(k)
-    if k == 1:
-        print("Enter file name\n")
-        strng = input()
-        client_socket.send(strng.encode())
-        size = client_socket.recv(1024).decode()
-        size = int(size)
-        print("The file size is - ", size, " bytes")
-        strng = client_socket.recv(size).decode()
-        print("\nThe contents of that file - ")
-        print(strng)
+    print("Enter file name")
+    fname = input()
 
-    if k == 2 or k == 3:
-        print("Enter file name of the image with extension (example: filename.jpg, filename.png or if a video file then filename.mpg etc) - ")
-        fname = input()
-        client_socket.send(fname.encode())
-        with open(fname, 'ab') as fp:
-            while True:
-                strng = client_socket.recv(512)
-                if not strng:
-                    break
-                fp.write(strng)
-        print("Data Received successfully")
+    # Hash the original image
+    img_hash = hash_image(fname)
+    client_socket.send(str(img_hash).encode())
+
+    # Receive server response
+    response = client_socket.recv(1024).decode()
+    if response == "Image is problematic":
+        print("Image is problematic")
+    else:
+        # Encrypt the image
+        encrypted_data = encrypt_image(fname)
         
-        # open the image
-        if os.name == 'nt':  # windows
-            os.system(f'start {fname}')
-        elif os.name == 'posix':  # linux/mac
-            os.system(f'open {fname}')
-        exit()
+        # Send the encrypted image file
+        client_socket.sendall(encrypted_data)
+        print("Data sent successfully")
+
+        received_encrypted_data = b''
+        while True:
+            strng = client_socket.recv(512)
+            if not strng:
+                break
+            received_encrypted_data += strng
+
+        print("data got agn")
+
+        # Decrypt the received encrypted data
+        decrypted_data = decrypt_image(received_encrypted_data)
+        print("decrypted")
+        
+        # Save the decrypted file
+        decrypted_fname = "decrypted_" + fname
+        with open(decrypted_fname, 'wb') as dfp:
+            dfp.write(decrypted_data)
+
+        print("Data received and decrypted successfully")
+    
+    exit()
--- serverfinal.py
+++ serverfinal.py
@@ -1,36 +1,62 @@
 import socket
+from PIL import Image
+import imagehash
 import os
+
+def setup_problematic_img(hashfunc=imagehash.phash):
+    directory = "problematic"  # directory of problematic images
+    img_formats = ('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.svg')
+    problematic_img_hashes = []
+
+    for file in os.listdir(directory):
+        filename = os.path.join(directory, file).lower()
+        if filename.endswith(img_formats):
+            try:
+                img_hash = hashfunc(Image.open(filename))
+                problematic_img_hashes.append(img_hash)
+            except Exception as e:
+                print('Problem:', e, 'with', filename)
+                continue
+    
+    return problematic_img_hashes
+
+def check_img(problematic_img_hashes, img_hash_str, hashfunc=imagehash.phash, threshold=5):
+    img_hash = imagehash.hex_to_hash(img_hash_str)
+    for p_hash in problematic_img_hashes:
+        if abs(img_hash - p_hash) <= threshold:
+            return True
+    return False
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("localhost", 5005))
 server_socket.listen(5)
 
-client_socket, address = server_socket.accept()
-print("Connected to - ", address, "\n")
+problematic_img_hashes = setup_problematic_img()
 
 while True:
-    choice = client_socket.recv(1024).decode()
-    choice = int(choice)
-    if choice == 1:
-        data = client_socket.recv(1024).decode()
-        print("The following data was received - ", data)
-        print("Opening file - ", data)
-        with open(data, 'r') as fp:
-            strng = fp.read()
-        size = os.path.getsize(data)
-        size = str(size)
-        client_socket.send(size.encode())
-        client_socket.send(strng.encode())
+    client_socket, address = server_socket.accept()
+    print("Connected to - ", address, "\n")
 
-    if choice == 2 or choice == 3:
-        data = client_socket.recv(1024).decode()
-        print("The following data was received - ", data)
-        print("Opening file - ", data)
-        with open(data, 'rb') as img:
-            while True:
-                strng = img.read(512)
-                if not strng:
-                    break
-                client_socket.send(strng)
-        print("Data sent successfully")
-        exit()
+    img_hash_str = client_socket.recv(1024).decode()
+
+    if check_img(problematic_img_hashes, img_hash_str):
+        client_socket.send("Image is problematic".encode())
+        print("Img is problematic")
+    else:
+        client_socket.send("Image is OK".encode())
+        print("Img is ok")
+        
+        # Receive the encrypted file
+        encrypted_data = b''
+        while True:
+            strng = client_socket.recv(512)
+            if not strng:
+                break
+            encrypted_data += strng
+
+        client_socket.sendall(encrypted_data)
+    
+    print("data sent")
+    client_socket.close()
+
+
